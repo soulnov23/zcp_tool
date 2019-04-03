815168e244d2f244da0fb63528c6ec2455c6f9bb
zhangchengpeng <314899581@qq.com>
2019-04-03 11:27:24 +0800

    null

--- comm/single_process.h
+++ comm/single_process.h
@@ -1,6 +1,6 @@
 #ifndef __SINGLE_PROCESS_H__
 #define __SINGLE_PROCESS_H__
 
-int single_process(const char* proc_name);
+int single_process(const char *proc_name);
 
 #endif
--- comm/tool.cpp
+++ comm/tool.cpp
@@ -8,90 +8,90 @@
 #define HEADER_VALUE	": "
  
 static int get_field(string &str, string &value)
-{   
-    if(str.size() ==0)
-    {	
-        return 0;
-    }
- 
-    string::size_type start = str.find(FIELD_FLAG);
-    if(start == string::npos || start !=0)
-    {
-        start= 0;
-    }
-    else
-    {
+{
+	if(str.size() ==0)
+	{	
+		return 0;
+	}
+
+	string::size_type start = str.find(FIELD_FLAG);
+	if(start == string::npos || start !=0)
+	{
+		start= 0;
+	}
+	else
+	{
 		start += strlen(FIELD_FLAG);
-    }
- 
-    string::size_type end = str.find(FIELD_FLAG, start);
-    if(end == string::npos)
-    {
-       	end = str.size();
-    }
- 
-    value = str.substr(start, end - start);
-    str = str.substr(end);
- 
-    return 1;
+	}
+
+	string::size_type end = str.find(FIELD_FLAG, start);
+	if(end == string::npos)
+	{
+		end = str.size();
+	}
+
+	value = str.substr(start, end - start);
+	str = str.substr(end);
+
+	return 1;
 }
  
 static int get_value(const string &str, string &name, string &value)
 {
-    string::size_type pos = str.find(VALUE_FLAG);
-    if(pos == string::npos)
-    {
-        return 0;
-    }
- 
-    name = str.substr(0, pos);
-    value = str.substr(pos + strlen(VALUE_FLAG));
- 
-    return 1;
+	string::size_type pos = str.find(VALUE_FLAG);
+	if(pos == string::npos)
+	{
+		return 0;
+	}
+
+	name = str.substr(0, pos);
+	value = str.substr(pos + strlen(VALUE_FLAG));
+
+	return 1;
 }
 
 
 void map2str(string &url, const record_t &record)
 {
-    for(record_t::const_iterator it = record.begin(); it != record.end(); it++)
-    {
+	for(record_t::const_iterator it = record.begin(); it != record.end(); it++)
+	{
 		url += it->first.c_str();
 		url += VALUE_FLAG;
 		url += it->second.c_str();	
 		url += FIELD_FLAG;
-    }
- 
-    if(url.size())
-    {
+	}
+
+	if(url.size())
+	{
 		string tmp(url.c_str(), url.size()-1);
 		url = tmp;
-    }
+	}
 }
 
 void map2header(string &header, const record_t &record)
 {
-    for(record_t::const_iterator it = record.begin(); it != record.end(); it++)
-    {
+	for(record_t::const_iterator it = record.begin(); it != record.end(); it++)
+	{
 		header += it->first.c_str();
 		header += HEADER_VALUE;
 		header += it->second.c_str();	
 		header += HEADER_FLAG;
-    }
+	}
 }
 
 void str2map(string &buf, record_t &record)
 {
-    string bvalue, fname, fvalue;
- 
-    string stbuf = buf;
-    while(get_field(stbuf, bvalue))
-    {
+	string bvalue, fname, fvalue;
+
+	string stbuf = buf;
+	while(get_field(stbuf, bvalue))
+	{
 		if(!get_value(bvalue, fname, fvalue))
 		{
 			continue;
 		}
 		record[fname]=fvalue; 
-    }
+	}
 }
 
 int get_time_now(string &str_now)
@@ -110,7 +110,7 @@ int get_time_now(string &str_now)
 	{
 		return -1;
 	}
-    return 0;
+	return 0;
 }
 
 void string_replace(string &data, const string &src, const string &dst)
@@ -127,20 +127,20 @@ void string_replace(string &data, const string &src, const string &dst)
 
 string lower(const string &src)
 {
-    string dst;
-    for(size_t i=0; i<src.length(); i++) 
+	string dst;
+	for(size_t i=0; i<src.length(); i++) 
 	{
-        dst += ((unsigned char)tolower(src.c_str()[i]));
-    }
-    return dst;
+		dst += ((unsigned char)tolower(src.c_str()[i]));
+	}
+	return dst;
 }
 
 string upper(const string &src)
 {
-    string dst;
-    for(size_t i=0; i<src.length(); i++) 
+	string dst;
+	for(size_t i=0; i<src.length(); i++) 
 	{
-        dst += ((unsigned char)toupper(src.c_str()[i]));
-    }
-    return dst;
+		dst += ((unsigned char)toupper(src.c_str()[i]));
+	}
+	return dst;
 }
