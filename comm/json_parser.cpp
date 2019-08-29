cc32b1bc824aa0c13d57fe26da4e746081c6e172
zhangchengpeng <314899581@qq.com>
2019-08-29 21:04:41 +0800

    增加rapidjson的map和string转换

--- comm/json_parser.cpp
+++ comm/json_parser.cpp
@@ -3,6 +3,10 @@
 #include "utils.h"
 #include "json/reader.h"
 #include "json/writer.h"
+#include "rapidjson.h"
+#include "document.h"
+#include "writer.h"
+#include "stringbuffer.h"
 #include <fstream>
 using namespace std;
 
@@ -35,7 +39,31 @@ int json_to_map(map<string, string> &record, string &data)
 	Json::Value::Members member = value.getMemberNames();
 	for (Json::Value::Members::iterator it = member.begin(); it != member.end(); it++)
 	{
-		record.insert(pair<string, string>(*it, value[*it].asString()));
+		if (value[*it].isString())
+		{
+			//record.insert(pair<string, string>(*it, value[*it].asString()));
+			record[*it] = value[*it].asString();
+		}
+		else if (value[*it].isDouble())
+		{
+			//record.insert(pair<string, string>(*it, to_string(value[*it].asDouble())));
+			record[*it] = to_string(value[*it].asDouble());
+		}
+		else if (value[*it].isBool())
+		{
+			//record.insert(pair<string, string>(*it, to_string(value[*it].asBool())));
+			record[*it] = to_string(value[*it].asBool());
+		}
+		else if (value[*it].isInt())
+		{
+			//record.insert(pair<string, string>(*it, to_string(value[*it].asInt())));
+			record[*it] = to_string(value[*it].asInt());
+		}
+		else if (value[*it].isUInt())
+		{
+			//record.insert(pair<string, string>(*it, to_string(value[*it].asUInt())));
+			record[*it] = to_string(value[*it].asUInt());
+		}
 	}
 	return 0;
 }
@@ -50,6 +78,67 @@ void map_to_json(string &data, const map<string, string> &record)
 	}
 	Json::FastWriter writer;
 	data = writer.write(object);
-	//string_replace(data, "\n", "");
-	//string_replace(data, "\t", "");
+}
+
+int rapid_json_to_map(map<string, string> &record, string &data)
+{
+	rapidjson::Document content_json_doc;
+	content_json_doc.Parse(data.c_str());
+	if (content_json_doc.HasParseError() || !content_json_doc.IsObject())
+	{
+		PRINTF_ERROR("json error [%s]", data.c_str());
+        return -1;
+	}
+	for (rapidjson::Value::ConstMemberIterator it = content_json_doc.MemberBegin(); it != content_json_doc.MemberEnd(); it++)
+	{
+		if (it->value.IsInt())
+		{
+			record[it->name.GetString()] = to_string(it->value.GetInt());
+		}
+		else if (it->value.IsInt64())
+		{
+			record[it->name.GetString()] = to_string(it->value.GetInt64());
+		}
+		else if (it->value.IsBool())
+		{
+			record[it->name.GetString()] = to_string(it->value.GetBool());
+		}
+		else if (it->value.IsUint())
+		{
+			record[it->name.GetString()] = to_string(it->value.GetUint());
+		}
+		else if (it->value.IsUint64())
+		{
+			record[it->name.GetString()] = to_string(it->value.GetUint64());
+		}
+		else if (it->value.IsDouble())
+		{
+			record[it->name.GetString()] = to_string(it->value.GetDouble());
+		}
+		else if (it->value.IsString())
+		{
+			record[it->name.GetString()] = it->value.GetString();
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return 0;
+}
+
+void rapid_map_to_json(string &data, const map<string, string> &record)
+{
+	rapidjson::Document document;
+    document.SetObject();
+    rapidjson::Document::AllocatorType& allocator = document.GetAllocator();
+	map<string, string>::const_iterator it;
+	for (it = record.begin(); it != record.end(); it++)
+	{
+		document.AddMember(rapidjson::StringRef(it->first.c_str()), rapidjson::StringRef(it->second.c_str()), allocator);
+	}
+	rapidjson::StringBuffer buffer;
+  	rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+  	document.Accept(writer);
+  	data = buffer.GetString();
 }
