3dba842d14debb358a655aecf273de14856e199f
zhangchengpeng <314899581@qq.com>
2019-09-07 20:50:36 +0800

    修改server的读取配置文件

--- bin/start.sh
+++ bin/start.sh
@@ -9,7 +9,7 @@ if [ $procs -ge 1 ];then
 	echo "zcp_server already runing!"
 	exit 0
 else
-	./zcp_server ../conf/server.xml 1>>../log/zcp_server.log 2>&1
+	./zcp_server ../conf/server.conf.xml 1>>../log/zcp_server.log 2>&1
 	procs=`ps -ef | grep zcp_server | grep -v grep | grep -v vi | grep -v tail | grep -v kill -c`
 	if [ $procs -lt 1 ];then
 		echo "start zcp_server failed!"
