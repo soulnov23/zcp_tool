1043ae454aa3c97a8415d38b0cdb9f24b389a7af
zhangchengpeng <314899581@qq.com>
2019-09-07 20:51:09 +0800

    初步的server

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,46 @@
 #include "printf.h"
-#include "daemon.h"
-#include <iostream>
-#include <vector>
-#include <map>
-using namespace std;
-#include "utils.h"
-#define STRING(X) #X
-#define AND(X) X##X
+#include "xml_parser.h"
+#include <unistd.h>
+
+map<string, string> config;
 
 int main(int argc, char *argv[])
 {
-	/*
+	PRINTF_DEBUG("%s %s", argv[0], argv[1]);
 	if (argc < 2)
 	{
-		cerr << "Usage: " << argv[0] << " conf_file" << endl;
-		cerr << "Sample: " << argv[0] << " ../conf/test.conf.xml" << endl;
+		PRINTF_ERROR("Usage: %s conf_file", argv[0]);
+		PRINTF_ERROR("Sample: %s ../conf/test.conf.xml", argv[0]);
 		return -1;
 	}
-	*/
-	PRINTF_DEBUG("server start success");
-	//init_daemon(NULL);
-	const char *str = STRING(test);
-	cout << str << endl;
-	int num = AND(8);
-	cout << num << endl;
-	PRINTF_DEBUG("TEST:%d", 10);
-	while (true)
+
+	const char *file_path = argv[1];
+	if (xml_to_map(config, file_path) != 0)
 	{
-		;
+		PRINTF_ERROR("xml_to_map error");
+		return -1;
+	}
+	PRINTF_DEBUG("ip:%s port:%s count:%s", config["ip"].c_str(), config["port"].c_str(), config["count"].c_str());
+
+	int count = strtol(config["count"].c_str(), nullptr, 10);
+	for (int i = 0; i < count; i++)
+	{
+		__pid_t pid = fork();
+		if (pid == -1)
+		{
+			PRINTF_ERROR("fork error");
+			return -1;
+		}
+		else if (pid == 0)
+		{
+			//子进程
+
+		}
+		else //pid > 0
+		{
+			//父进程
+
+		}
 	}
 	return 0;
 }
