f269934894eb4c2a8aacc9b26889455e962dad62
zhangchengpeng <314899581@qq.com>
2019-10-08 17:08:26 +0800

    修改编码规范

--- comm/module.cpp
+++ comm/module.cpp
@@ -4,8 +4,7 @@
 
 typedef int (*FUNC)(int);
 
-int get_lib_func(const char *file_path, const char *func_name)
-{
+int get_lib_func(const char *file_path, const char *func_name) {
 	void *handle = dlopen(file_path, RTLD_LAZY); 
 	if (!handle) {
 		PRINTF_ERROR();
--- src/main.cpp
+++ src/main.cpp
@@ -5,40 +5,33 @@
 
 map<string, string> config;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	PRINTF_DEBUG("%s %s", argv[0], argv[1]);
-	if (argc < 2)
-	{
+	if (argc < 2) {
 		PRINTF_ERROR("Usage: %s conf_file", argv[0]);
 		PRINTF_ERROR("Sample: %s ../conf/test.conf.xml", argv[0]);
 		return -1;
 	}
 
 	const char *file_path = argv[1];
-	if (xml_to_map(config, file_path) != 0)
-	{
+	if (xml_to_map(config, file_path) != 0) {
 		PRINTF_ERROR("xml_to_map error");
 		return -1;
 	}
 	PRINTF_DEBUG("ip:%s port:%s count:%s", config["ip"].c_str(), config["port"].c_str(), config["count"].c_str());
 
 	//int count = strtol(config["count"].c_str(), nullptr, 10);
-	if (init_signal() != 0)
-	{
+	if (init_signal() != 0) {
 		PRINTF_ERROR("init_signal error");
 		return -1;
 	}
-	for (int i = 0; i < 1; i++)
-	{
-		if (fork_child() != 0)
-		{
+	for (int i = 0; i < 1; i++) {
+		if (fork_child() != 0) {
 			PRINTF_ERROR("fork_child error");
 			return -1;
 		}
 	}
-	while (true)
-	{
+	while (true) {
 		
 	}
 	return 0;
