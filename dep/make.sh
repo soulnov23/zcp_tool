9e759d946bafb8c57e1102d4e3f6fd27e403b790
zhangchengpeng <314899581@qq.com>
2020-03-10 20:20:48 +0800

    curl依赖openssl，不能make完了openssl马上就清理

--- dep/make.sh
+++ dep/make.sh
@@ -19,15 +19,18 @@ function install_open_ssl()
 	make
 	make install
 	cp -f /tmp/ssl/lib/libcrypto.* /tmp/ssl/lib/libssl.* $2
+}
+
+function clean_open_ssl()
+{
+	cd $1
 	make clean
 	rm -rf /tmp/ssl
 }
 
 function uninstall_open_ssl()
 {
-	cd $1
-	make clean
-	rm -rf /tmp/ssl
+	clean_open_ssl $1
 	rm -rf $2/libcrypto.* $2/libssl.*
 }
 
@@ -39,27 +42,18 @@ function install_curl()
 	make
 	make install
 	cp -f /tmp/curl/lib/libcurl.* $2
+}
+
+function clean_curl()
+{
+	cd $1
 	make clean
-	rm -rf ./docs/examples/.deps
-	rm -rf ./lib/.deps
-	rm -rf ./src/.deps
-	rm -rf ./tests/libtest/.deps
-	rm -rf ./tests/server/.deps
-	rm -rf ./tests/unit/.deps
 	rm -rf /tmp/curl
 }
 
 function uninstall_curl()
 {
-	cd $1
-	make clean
-	rm -rf ./docs/examples/.deps
-	rm -rf ./lib/.deps
-	rm -rf ./src/.deps
-	rm -rf ./tests/libtest/.deps
-	rm -rf ./tests/server/.deps
-	rm -rf ./tests/unit/.deps
-	rm -rf /tmp/curl
+	clean_curl $1
 	rm -rf $2/libcurl.*
 }
 
@@ -70,13 +64,17 @@ function install_libco()
 	make
 	cp ./lib/* $2
 	cp ./solib/* $2
+}
+
+function clean_libco()
+{
+	cd $1
 	make clean
 }
 
 function uninstall_libco()
 {
-	cd $1
-	make clean
+	clean_libco $1
 	rm -rf $2/libcolib.*
 }
 
@@ -92,15 +90,18 @@ function install_gperf()
 	make
 	make install
 	cp -f /tmp/gperf/lib/libtcmalloc*.* $2/
+}
+
+function clean_gperf()
+{
+	cd $1
 	make clean
 	rm -rf /tmp/gperf
 }
 
 function uninstall_gperf()
 {
-	cd $1
-	make clean
-	rm -rf /tmp/gperf
+	clean_gperf $1
 	rm -rf $2/libtcmalloc*.*
 }
 
@@ -110,11 +111,17 @@ main()
 	install_open_ssl)
 		install_open_ssl $2 $3
 		;;
+	clean_open_ssl)
+		clean_open_ssl $2
+		;;
 	uninstall_open_ssl)
 		uninstall_open_ssl $2 $3
 		;;
 	install_curl)
 		install_curl $2 $3
+		;;
+	clean_curl)
+		clean_curl $2
 		;;
 	uninstall_curl)
 		uninstall_curl $2 $3
@@ -122,11 +129,17 @@ main()
 	install_libco)
 		install_libco $2 $3
 		;;
+	clean_libco)
+		clean_libco $2
+		;;
 	uninstall_libco)
 		uninstall_libco $2 $3
 		;;
 	install_gperf)
 		install_gperf $2 $3
+		;;
+	clean_gperf)
+		clean_gperf $2
 		;;
 	uninstall_gperf)
 		uninstall_gperf $2 $3
