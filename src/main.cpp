e6ee8e870a2567719b491eb03b2785336b8acf23
zhangchengpeng <314899581@qq.com>
2020-07-13 17:47:10 +0800

    简易多进程模型

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,26 @@
 
 map<string, string> config;
 
+int fork_child() {
+    __pid_t pid = fork();
+    if (pid == -1) {
+        PRINTF_ERROR("fork error");
+        return -1;
+    } else if (pid == 0) {
+        //子进程
+        server test;
+        test.start();
+        //退出，不然还会接着执行for循环创建更多的子进程的子进程
+        _exit(0);
+    }
+    // pid > 0
+    else {
+        //父进程
+        PRINTF_DEBUG("fork child pid:%d", pid);
+    }
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
     PRINTF_DEBUG("%s %s", argv[0], argv[1]);
     if (argc < 2) {
@@ -22,25 +42,15 @@ int main(int argc, char* argv[]) {
                  config["port"].c_str(), config["count"].c_str());
 
     int count = strtol(config["count"].c_str(), nullptr, 10);
-    if (init_signal() != 0) {
-        PRINTF_ERROR("init_signal error");
-        return -1;
-    }
-    /*
-          if (init_pid_file() != 0) {
-                  PRINTF_ERROR("init_pid_file error");
-                  return -1;
-          }
-  */
-    PRINTF_DEBUG("TODO start");
+    
     for (int i = 0; i < count; i++) {
         if (fork_child() != 0) {
             PRINTF_ERROR("fork_child error");
             return -1;
         }
     }
-    PRINTF_DEBUG("TODO end");
     while (true) {
+        sleep(10);
     }
     return 0;
 }
--- src/server.h
+++ src/server.h
@@ -1,9 +1,42 @@
-#ifndef __SERVER_H_
-#define __SERVER_H_
+#ifndef __SERVER_H__
+#define __SERVER_H__
 
-int init_signal();
-int init_pid_file();
-void force_exit();
-int fork_child();
+#include <map>
+#include <memory>
+using namespace std;
+#include "net/connector.h"
+#include "net/net_utils.h"
+
+class server {
+public:
+    server();
+    ~server();
+
+    int start();
+
+private:
+    void stop();
+
+private:
+    static void signal_handle_func(int sig_no, siginfo_t* sig_info, void* data);
+
+private:
+    int init_pid_file();
+    int init_signal();
+    int connect_timeout(int nsec, int usec);
+    int do_listen();
+    void do_accept();
+    void do_recv(int fd);
+    void do_send(int fd, const char* data, int len);
+    void event_loop();
+    void force_exit();
+
+private:
+    bool m_flag;
+    int m_epoll_fd;
+    int m_listen_fd;
+    int m_client_fd;
+    map<int, shared_ptr<connector>> m_fd_conn;
+};
 
 #endif
