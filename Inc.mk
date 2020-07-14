dccb0ff51643036da331ab747224f53d2fbde597
zhangchengpeng <314899581@qq.com>
2020-07-14 20:56:38 +0800

    AlignConsecutiveAssignments也不支持

--- Inc.mk
+++ Inc.mk
@@ -16,7 +16,7 @@ CXX = g++ -std=c++2a
 AR = ar
 ARFLAGS = -scurv
 RANLIB = ranlib
-FORMAT = clang-format --style="{BasedOnStyle: Google,AccessModifierOffset: -4,AlignConsecutiveAssignments: true,ColumnLimit: 130,UseTab: Never,IndentWidth: 4,TabWidth: 4}" -i
+FORMAT = clang-format --style="{BasedOnStyle: Google,AccessModifierOffset: -4,ColumnLimit: 130,UseTab: Never,IndentWidth: 4,TabWidth: 4}" -i
 
 CFLAGS ?=
 CXXFLAGS ?=
