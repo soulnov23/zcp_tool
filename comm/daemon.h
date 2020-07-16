04c8c97f5fa99056fdd3adf81ef174fcf7d73ba2
zhangchengpeng <314899581@qq.com>
2020-07-16 19:47:28 +0800

    模仿系统函数实现方式

--- comm/daemon.h
+++ comm/daemon.h
@@ -4,15 +4,16 @@
 /**
  * init_daemon - 将进程变成守护进程
  *
- * @param path 要切换到的当前工作目录。参数为NULL是切换到/tmp
+ * @param __nochdir 是否要切换当前工作目录到根目录
+ * @param __noclose 是否要重定向标准输入输出错误到/dev/null
  *
  * @return 0 成功
  *        -1 fork失败
  *        -2 改变当前目录失败
  *
- * @note 返回失败时，标准输入输出不会关闭
+ * @note 返回失败时，标准输入输出错误不会关闭
  **/
 
-int init_daemon(char* path);
+int init_daemon(int __nochdir, int __noclose);
 
 #endif
