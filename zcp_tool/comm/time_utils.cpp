fa6f9b708d3bbec5dd22971b215e8932b4adc7cc
zhangchengpeng <314899581@qq.com>
2021-01-28 14:45:47 +0800

    C++支持Return value optimization 编译器会优化函数返回值，不会再生成临时变量给外部，减少一次拷贝 外部获取函数返回值赋值的时候，最好使用右值引用，或者左值引用，再减少一次拷贝

--- zcp_tool/comm/time_utils.cpp
+++ zcp_tool/comm/time_utils.cpp
@@ -22,26 +22,25 @@ int get_time_now(string& str_now) {
 }
 */
 
-int get_time_now(string& str_now) {
+string get_time_now() {
+    char buf[128] = "0000-00-00 00:00:00 000000";
     struct timeval tv;
     struct tm timestamp;
     if (gettimeofday(&tv, nullptr) == -1) {
         PRINTF_ERROR("gettimeofday failed.");
-        return -1;
+        return buf;
     }
     time_t now = tv.tv_sec;
     if (localtime_r(&now, &timestamp) == nullptr) {
         PRINTF_ERROR("localtime_r failed.");
-        return -1;
+        return buf;
     }
-    char buf[128];
     if (snprintf(buf, sizeof(buf), "%04d-%02d-%02d %02d:%02d:%02d %06d", timestamp.tm_year + 1900, timestamp.tm_mon + 1,
                  timestamp.tm_mday, timestamp.tm_hour, timestamp.tm_min, timestamp.tm_sec, (int)(tv.tv_usec)) < 0) {
         PRINTF_ERROR("snprintf failed.");
-        return -1;
+        return buf;
     }
-    str_now = buf;
-    return 0;
+    return buf;
 }
 
 time_t get_time_sec() {
@@ -61,39 +60,40 @@ time_t get_time_usec() {
     return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
 
-int get_time_zone(int& time_zone) {
+int get_time_zone() {
+    int time_zone = 0;
     time_t t1, t2;
     struct tm tm_local, tm_utc;
     struct timeval tv;
     if (gettimeofday(&tv, nullptr) == -1) {
         PRINTF_ERROR("gettimeofday failed.");
-        return -1;
+        return time_zone;
     }
     time_t now = tv.tv_sec;
 
     if (localtime_r(&now, &tm_local) == nullptr) {
         PRINTF_ERROR("localtime_r failed.");
-        return -1;
+        return time_zone;
     }
     if (gmtime_r(&now, &tm_utc) == nullptr) {
         PRINTF_ERROR("gmtime_r failed.");
-        return -1;
+        return time_zone;
     }
 
     tm_local.tm_isdst = -1;
     t1                = mktime(&tm_local);
     if (t1 == -1) {
         PRINTF_ERROR("mktime failed.");
-        return -1;
+        return time_zone;
     }
     tm_utc.tm_isdst = -1;
     t2              = mktime(&tm_utc);
     if (t2 == -1) {
         PRINTF_ERROR("mktime failed.");
-        return -1;
+        return time_zone;
     }
     time_zone = (t1 - t2) / 3600;
-    return 0;
+    return time_zone;
 }
 
 time_t str_time2date(const string& str_time) {
@@ -101,7 +101,7 @@ time_t str_time2date(const string& str_time) {
     if (sscanf(str_time.c_str(), "%04d-%02d-%02d %02d:%02d:%02d", &st_time.tm_year, &st_time.tm_mon, &st_time.tm_mday,
                &st_time.tm_hour, &st_time.tm_min, &st_time.tm_sec) == EOF) {
         PRINTF_ERROR("sscanf failed.");
-        return -1;
+        return 0;
     }
     st_time.tm_year -= 1900;
     st_time.tm_mon -= 1;
@@ -109,23 +109,22 @@ time_t str_time2date(const string& str_time) {
     time_t t         = mktime(&st_time);
     if (t == -1) {
         PRINTF_ERROR("mktime failed.");
-        return -1;
+        return 0;
     }
     return t;
 }
 
-string date2str_time(time_t time) {
+string date2str_time(const time_t& time) {
+    char date[30] = "0000-00-00 00:00:00";
     struct tm st_time;
     if (localtime_r(&time, &st_time) == nullptr) {
         PRINTF_ERROR("localtime_r failed.");
-        return "";
+        return date;
     }
-
-    char date[30];
     if (snprintf(date, sizeof(date), "%04d-%02d-%02d %02d:%02d:%02d", st_time.tm_year + 1900, st_time.tm_mon + 1, st_time.tm_mday,
                  st_time.tm_hour, st_time.tm_min, st_time.tm_sec) < 0) {
         PRINTF_ERROR("snprintf failed.");
-        return "";
+        return date;
     }
     return date;
 }
