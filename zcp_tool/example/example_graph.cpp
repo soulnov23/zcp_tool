e044943df49e71bac2eb85da9218e9fc964b5c9d
zhangchengpeng <314899581@qq.com>
2021-07-02 19:37:03 +0800

    暂时不增加gmock gtest库

---
+++ zcp_tool/example/example_graph.cpp
@@ -0,0 +1,48 @@
+#include "zcp_tool/comm/graph.h"
+#include "zcp_tool/comm/printf_utils.h"
+
+int main(int argc, char* argv[]) {
+    graph_adjvex_list_t list;
+    vertex_t* A = list.add_vertex("A");
+    vertex_t* B = list.add_vertex("B");
+    vertex_t* C = list.add_vertex("C");
+    vertex_t* D = list.add_vertex("D");
+    vertex_t* E = list.add_vertex("E");
+    vertex_t* F = list.add_vertex("F");
+    vertex_t* G = list.add_vertex("G");
+    vertex_t* H = list.add_vertex("H");
+
+    list.add_edge(A, B, "3");
+    list.add_edge(B, C, "5");
+    list.add_edge(B, D, "5");
+    list.add_edge(C, F, "10");
+    list.add_edge(E, B, "7");
+    list.add_edge(E, F, "7");
+    list.add_edge(F, G, "2");
+    list.add_edge(H, D, "6");
+    list.add_edge(H, F, "6");
+
+    string data = list.dump_dot();
+    PRINTF_DEBUG("%s", data.c_str());
+
+    if (list.find_edge("A", "B")) {
+        PRINTF_DEBUG("AB");
+    }
+    if (list.find_edge("B", "C")) {
+        PRINTF_DEBUG("BC");
+    }
+    if (list.find_edge("A", "C")) {
+        PRINTF_DEBUG("AC");
+    }
+
+    data = list.dfs_traverse_recursion();
+    PRINTF_DEBUG("%s", data.c_str());
+    data = list.dfs_traverse();
+    PRINTF_DEBUG("%s", data.c_str());
+    data = list.bfs_traverse();
+    PRINTF_DEBUG("%s", data.c_str());
+    data = list.topological_sort();
+    PRINTF_DEBUG("%s", data.c_str());
+
+    return 0;
+}
--- zcp_tool/example/test.cpp
+++ zcp_tool/example/test.cpp
@@ -1,48 +1,18 @@
-#include "zcp_tool/comm/graph.h"
-#include "zcp_tool/comm/printf_utils.h"
+#include <map>
+#include <vector>
+
+#include "third_party/fmt/ranges.h"
 
 int main(int argc, char* argv[]) {
-    graph_adjvex_list_t list;
-    vertex_t* A = list.add_vertex("A");
-    vertex_t* B = list.add_vertex("B");
-    vertex_t* C = list.add_vertex("C");
-    vertex_t* D = list.add_vertex("D");
-    vertex_t* E = list.add_vertex("E");
-    vertex_t* F = list.add_vertex("F");
-    vertex_t* G = list.add_vertex("G");
-    vertex_t* H = list.add_vertex("H");
-
-    list.add_edge(A, B, "3");
-    list.add_edge(B, C, "5");
-    list.add_edge(B, D, "5");
-    list.add_edge(C, F, "10");
-    list.add_edge(E, B, "7");
-    list.add_edge(E, F, "7");
-    list.add_edge(F, G, "2");
-    list.add_edge(H, D, "6");
-    list.add_edge(H, F, "6");
-
-    string data = list.dump_dot();
-    PRINTF_DEBUG("%s", data.c_str());
-
-    if (list.find_edge("A", "B")) {
-        PRINTF_DEBUG("AB");
-    }
-    if (list.find_edge("B", "C")) {
-        PRINTF_DEBUG("BC");
-    }
-    if (list.find_edge("A", "C")) {
-        PRINTF_DEBUG("AC");
-    }
-
-    data = list.dfs_traverse_recursion();
-    PRINTF_DEBUG("%s", data.c_str());
-    data = list.dfs_traverse();
-    PRINTF_DEBUG("%s", data.c_str());
-    data = list.bfs_traverse();
-    PRINTF_DEBUG("%s", data.c_str());
-    data = list.topological_sort();
-    PRINTF_DEBUG("%s", data.c_str());
-
+    std::vector<std::string> vec;
+    vec.push_back("a");
+    vec.push_back("b");
+    vec.push_back("c");
+    fmt::print("{}\n", vec);
+    std::map<std::string, std::string> record;
+    record["a"] = "1";
+    record["b"] = "2";
+    record["c"] = "3";
+    fmt::print("{}\n", record);
     return 0;
 }
