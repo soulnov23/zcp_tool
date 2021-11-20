4facc6b1b5e0c512bb53715aa7be7f4e61bbb69d
zhangchengpeng <314899581@qq.com>
2021-11-20 18:20:18 +0800

    通过yaml文件写系统配置，不走xml

--- release/bin/restart.sh
+++ release/bin/restart.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-curtime=`date "+%Y-%m-%d %H:%M:%S"`
-echo "[$curtime] [echo] restart..."
-
 ./stop.sh
+curtime=`date "+%Y-%m-%d %H:%M:%S"`
 echo "[$curtime] [echo] sleep 5s..."
 sleep 5
 ./start.sh
-
-echo "[$curtime] [echo] restart zcp_server success"
--- release/bin/start.sh
+++ release/bin/start.sh
@@ -10,7 +10,7 @@ if [ $procs -ge 1 ];then
 	echo "[$curtime] [echo] zcp_server already runing!"
 	exit 0
 else
-	./zcp_server ../conf/server.conf.xml 1>>../log/zcp_server.log 2>&1
+	./zcp_server ../conf/server.yaml 1>>../log/zcp_server.log 2>&1
 	procs=`ps -ef | grep zcp_server | grep -v grep | grep -v vi | grep -v tail | grep -v kill -c`
 	if [ $procs -lt 1 ];then
 		echo "[$curtime] [echo] start zcp_server failed!"
