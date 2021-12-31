c4cc45adddfcacb20db36554c02d79a74f97c02c
chengpzhang <chengpzhang@tencent.com>
2021-12-31 15:38:28 +0800

    调整链接库

--- src/base/uuid_utils.h
+++ src/base/uuid_utils.h
@@ -2,9 +2,18 @@
 
 #include <string>
 
+#include "uuid.h"
+
 /*
 uuid_generate_random使用高质量的随机数生成器/dev/urandom或者/dev/random
 uuid_generate_time使用当前时间和本地以太网mac地址
 uuid_generate根据/dev/urandom或者/dev/random是否可用，优先选择uuid_generate_random，其次uuid_generate_time
 */
-std::string uuid_generate();
+std::string uuid_generate() {
+    uuid_t uuid;
+    char temp_uuid[64];
+    uuid_generate(uuid);
+    uuid_unparse_lower(uuid, temp_uuid);
+    std::string str_uuid(temp_uuid);
+    return str_uuid;
+}
