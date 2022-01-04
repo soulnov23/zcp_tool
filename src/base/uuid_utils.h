576a4d0a9bc95455afe315db89717013c87ae96c
chengpzhang <chengpzhang@tencent.com>
2022-01-04 15:23:58 +0800

    增加uuid生成

--- src/base/uuid_utils.h
+++ src/base/uuid_utils.h
@@ -7,7 +7,7 @@
 /*
 uuid_generate_random使用高质量的随机数生成器/dev/urandom或者/dev/random
 uuid_generate_time使用当前时间和本地以太网mac地址
-uuid_generate根据/dev/urandom或者/dev/random是否可用，优先选择uuid_generate_random，其次uuid_generate_time
+uuid_generate根据/dev/urandom或者/dev/random是否可用，可用调用uuid_generate_random，不可用就调用uuid_generate_time
 */
 std::string uuid_generate() {
     uuid_t uuid;
