1ddc7f9aea4c909cf7962cf2e454082d2fabd017
chengpzhang <chengpzhang@tencent.com>
2022-01-14 21:34:16 +0800

    完善NRVO的注释

--- src/base/map_util.h
+++ src/base/map_util.h
@@ -16,6 +16,6 @@ value_t map_get_value(const map_t& map, const key_t& key) {
 template <typename map_t, typename key_t = typename map_t::key_type, typename value_t = typename map_t::mapped_type>
 value_t map_get_value(const map_t& map, const key_t& key, value_t&& default_value) {
     auto pos = map.find(key);
-    // 使用std::forward完美转发保持default_value的右值右引用属性，这样在外部调map_get_value赋值的时候保证可以触发移动赋值运算符，而不是复制运算符，少一次拷贝
+    // default_value不是临时对象，不会触发NRVO，使用std::forward完美转发保持default_value的右值右引用属性，这样在外部调map_get_value赋值的时候保证可以触发移动赋值运算符，而不是复制运算符，少一次拷贝
     return (pos != map.end()) ? (pos->second) : value_t(std::forward<value_t>(default_value));
 }
