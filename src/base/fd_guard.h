c65554e18ddd0ede9c92832a47b980b80f462718
chengpzhang <chengpzhang@tencent.com>
2022-02-10 15:08:29 +0800

    增加默认构造和等号赋值运算符重载

--- src/base/fd_guard.h
+++ src/base/fd_guard.h
@@ -9,15 +9,20 @@ class fd_guard {
     CLASS_UNMOVABLE(fd_guard)
 
 public:
+    fd_guard() : fd_(-1) {}
     explicit fd_guard(int fd) : fd_(fd) {}
-
     ~fd_guard() {
+        if (fd_ < 0) {
+            return;
+        }
         close(fd_);
         fd_ = -1;
     }
 
     // 支持fd_guard类型到int类型的隐式转换
     operator int() { return fd_; }
+    // 支持fd_guard等号赋值int类型
+    void operator=(int fd) { fd_ = fd; }
 
 private:
     int fd_;
